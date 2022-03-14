518a4ca50bf69b38fa7a7d69e9f95497aed13b7d
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-14 15:23:59 -0400

    add function to deleted a noted

--- server.js
+++ server.js
@@ -62,3 +62,23 @@ app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, notes)
     res.json(newNote)
 })
+
+//Creating the function to deleted a note
+function deleteNote (id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if(note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(path.join(__dirname, './db/db.json'),
+            JSON.stringify({notes: notesArray}, null, 2)
+            );
+            break;
+        }
+    }
+}
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes)
+    res.json(true)
+
+});
