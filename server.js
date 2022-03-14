036f40c74de1d310051df8409dbb754649044103
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-14 14:08:37 -0400

    added heroku to the repository

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const {notes} = require('./db/db.json');
 const path = require('path');
 const fs = require('fs');
 
+//Setting the server and initiating the app
+const app = express();
+const PORT = 
