feab18014c8bdeb201580f7ef960e15f48f37dad
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-14 15:15:42 -0400

    add parsing the roots

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ const fs = require('fs');
 //Setting the server and initiating the app
 const app = express();
 const PORT = process.env.PORT || 3000
+
+//Parse incoming string or array data
+app.use(express.urlencoded( {extended: true}));
+//Parse incoming json data
+app.use(express.json());
+//Parse the path route 
+app.use(express.static(path.join(__dirname, 'public')));
+
